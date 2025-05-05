e36a1764b4c35cb0daa77cbcc0352fdd50330aa7
Md. Rajuan Hossen <rajuan.official@gmail.com>
2025-05-06 00:37:40 +0600

    js-updaste

--- script.js
+++ script.js
@@ -1,56 +1,71 @@
-// console.log("there is the javascript");
-// async function getSongs(){
-//     let a = await  fetch("http://127.0.0.1:5500/songs/")
-//     let response = await  a.text();
-//     let div = document.createElement('div')
-//     div.innerHTML = response
-//     let as = div.getElementsByTagName('a')
-//     let songs = []
-//     for (let index = 0; index < as.length.length; index++) {
-//         const element = as[index]
-//         if (element.href.endsWith(".mp3")) {
-//             songs.push(Element.href)
-//         }
-//     }
-//     return songs
-// }
+let currentSong = new Audio();
 
-// async function main(){
 
-//     let songs = await getSongs()
-//     console.log(songs);
-    
-// }
 
-// main()
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs");
+  let response = await a.text();
+  // console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
 
-async function getSongs(){
-
-    let a = await fetch("http://127.0.0.1:5500/songs/")
-    let response = await a.text()
-    // console.log(response);
-    let div = document.createElement('div')
-    div.innerHTML = response
-    let as = div.getElementsByTagName("a")
-    // console.log(as);
-
-    let songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
-        }
+  let as = div.getElementsByTagName("a");
+  let songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith("mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
     }
-    return songs
-}   
-async function main(){
-    let songs = await getSongs()
-    console.log(songs);
-    var audio = new Audio(songs[0]);
-    audio.play();
-    playMusic(songs[0])
-    
+  }
+  return songs;
 }
 
-main()
+async function main() {
+  //get all songs
+  let songs = await getSongs();
+  // console.log(songs);
 
+  //show all the songs of playlist
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li>                        
+                            <img class="invert" width="34" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>Rajuan</div>
+                                </div>
+                                <div class="playnow">
+                                    <span>Play Now</span>
+                                    <img class="invert" src="img/play.svg" alt="">
+                                </div>
+                        </li>`;
+  }
+
+
+  //event listener for all song
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
+  });
+
+  // audio.addEventListener("loadeddata", () => {
+  //   let duration = audio.duration;
+  //   console.log(duration);
+  // });
+  return songs
+}
+    
+const playMusic = (track)=>{
+  // var audio = new Audio('/songs/' + track);
+  currentSong.src = "/songs/" + track
+  currentSong.play();
+
+}
+main();
